afa8736205429b38ac33c05be8996c6bf784059f
Miles Ross <Milesross24@gmail.com>
2022-10-16 22:08:14 -0500

    passes autograder

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Write your JavaScript code here!
+// not fetching scipt?? 
+const { addDestinationInfo, formSubmission } = require("./scriptHelper");
+const { myFetch } = require("/scriptHelper");
 
 window.addEventListener("load", function() {
 
@@ -11,6 +14,23 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       let newPlanet = pickPlanet (listedPlanets);
+       addDestinationInfo(document, newPlanet.name, newPlanet.diameter, newPlanet.star, newPlanet.distance, newPlanet.moons, newPlanet.imageUrl);
    })
+   let list = document.getElementById("faultyItems");
+   list.style.visibiltity = "hidden";
+   let form = document.querySelector('form');
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    // preventDefault()??
+
+    let pilot = document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+
+    formSubmission(document, list, copilot, fuelLevel, cargoLevel);
+
+});
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,41 +1,98 @@
 // Write your helper functions here!
+
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
+   let missonObjective = document.getElementById('missionTarget');
+   missonObjective.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name} </li>
+                    <li>Diameter: ${diameter} </li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance} </li>
+                    <li>Number of Moons: ${moons} </li>
                 </ol>
-                <img src="">
-   */
-}
+                <img src="${imageUrl}">
+                `;
+};
 
 function validateInput(testInput) {
-   
-}
+    let numInput= Number(testInput);
+    if (testInput === "") {
+        return "Empty";
+    } else if (isNaN(numInput)) {
+        return "Not a Number";
+    } else if (isNaN(numInput)=== false) {
+        return "Is a Number";
+    }
+};
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    let cargoStatus = document.getElementById('cargoStatus');
+   let pilotStatus = document.getElementById('pilotStatus');
+   let fuelStatus = document.getElementById('fuelStatus');
+   let copilotStatus = document.getElementById('copilotStatus');
+
+   if (validateInput(pilot) === 'Empty' || validateInput(copilot) === 'Empty' || 
+   validateInput(fuelLevel) === 'Empty' || validateInput(cargoLevel) === 'Empty') {
+       alert ('All fields are required');
+   } else if (validateInput(fuelLevel) === 'Not a Number' || validateInput(cargoLevel) === 'Not a Number') {
+       alert('Please enter numerical values for Fuel Level and Cargo Mass');
+   } else if (validateInput(pilot)==='Is a Number'||validateInput(copilot)==='Is a Number') {
+       alert('Please do not enter numbers for name of pilot or co-pilot');
+   } 
+   else {
+       list.style.visibility = 'visible';
+pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+let launchStatus = document.getElementById("launchStatus");
+
+if (fuelLevel < 10000 && cargoLevel <= 10000) {
+    fuelStatus.innerHTML = 'Fuel level too low for launch';
+    cargoStatus.innerHTML = 'Cargo mass low enough for launch';
+    launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
+    launchStatus.style.color = "#C7254E";
+   } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
+    cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
+    fuelStatus.innerHTML = 'Fuel level high enough for launch';
+    launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
+    launchStatus.style.color = "#C7254E";
+   } else if (fuelLevel < 10000 && cargoLevel > 10000) {
+    cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
+    fuelStatus.innerHTML = 'Fuel level too low for launch';
+    launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
+    launchStatus.style.color = "#C7254E";
+   }
+    else {
+    fuelStatus.innerHTML = 'Fuel level high enough for launch';
+    cargoStatus.innerHTML = 'Cargo mass low enough for launch';
+    launchStatus.innerHTML = 'Shuttle is Ready for Launch';
+    launchStatus.style.color = "#419F6A";
+   }
+}
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        if (response.status >= 400) {
+            throw new Error ("Bad response");
+        }
+        else {
+        return response.json() 
+        }
+    });
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let location = Math.floor(Math.random()* planets.length);
+    return planets[location];
 }
-
+// is this breaking my code with my return statment outside the function??
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
